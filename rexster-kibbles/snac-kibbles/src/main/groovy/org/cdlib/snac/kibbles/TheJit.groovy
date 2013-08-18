a65e0abb26aa54adb89875a5efb76677c1dfe104
Brian Tingle <brian.tingle.cdlib.org@gmail.com>
2013-08-18 14:14:58 -0700

    move snac-kibbles into this repository
    lets us include ../pom.xml

---
+++ rexster-kibbles/snac-kibbles/src/main/groovy/org/cdlib/snac/kibbles/TheJit.groovy
@@ -0,0 +1,71 @@
+package org.cdlib.snac.kibbles;
+
+// The JSON this produces is used by this HTML/js
+// http://code.google.com/p/xtf-cpf/source/browse/cpf2html/snac-jit.html?name=xtf-cpf
+// http://code.google.com/p/xtf-cpf/source/browse/cpf2html/snac-jit.js?name=xtf-cpf
+
+import com.tinkerpop.blueprints.Edge;
+import com.tinkerpop.blueprints.Graph;
+import com.tinkerpop.blueprints.Vertex;
+import com.tinkerpop.rexster.extension.*;
+import com.tinkerpop.rexster.RexsterResourceContext;
+import org.codehaus.jettison.json.JSONObject;
+import org.codehaus.jettison.json.JSONArray;
+// import com.tinkerpop.blueprints.util.json.GraphSONWriter;
+import com.tinkerpop.gremlin.groovy.Gremlin;
+import javax.ws.rs.core.Response;
+
+@ExtensionNaming(namespace = "snac", name = "theJit")
+public class TheJit extends AbstractRexsterExtension {
+
+    static {
+        Gremlin.load();
+    }
+
+    @ExtensionDefinition(extensionPoint = ExtensionPoint.VERTEX)
+    @ExtensionDescriptor(description = "JSON needed for graphs in the JavaScript InfoVis Toolkit")
+    public ExtensionResponse evaluate(@RexsterContext RexsterResourceContext context,
+                                      @RexsterContext Vertex v) {
+
+        // JSON format http://thejit.org/static/v20/Docs/files/Loader/Loader-js.html
+        JSONArray output = new JSONArray();
+
+        // popular graph neighbors get aggregated into this array
+        def neighbors = [];
+
+        // collect neighbors; sorted by "popularity"
+        // .score was precomputed with: for (z in g.V ) { z.score = z.out.count() }
+        v.out.dedup.sort{-it.score}._()[0..49].aggregate(neighbors).iterate();
+
+        // add JSON for the "center" node to the output array first
+        output.put(buildNode(v, neighbors));
+
+        // for each popular graph neighbor; build neighbors JSON and add to output array
+        neighbors.each{ output.put(buildNode(it, neighbors)); }
+
+        // return the results
+        return new ExtensionResponse(Response.ok(output).build());
+    }
+
+    // build the JSON for the node
+    private buildNode(vertex, neighbors) {
+        def node = new JSONObject();
+        node.put("id", vertex.id as String);
+        node.put("name", vertex.identity);
+        node.put("filename", vertex.filename);
+        node.put("adjacencies", buildAdjacencies(vertex, neighbors));
+        return node;
+    }
+
+    // build array of adjacent nodes
+    private buildAdjacencies(vertex, neighbors) {
+        JSONArray collect = new JSONArray();
+        def self = [];
+        vertex.aggregate(self).iterate();
+        vertex.both.dedup().retain(neighbors).except(self).each {
+            collect.put(it.id as String);
+        };
+        return collect;
+    }
+
+}
